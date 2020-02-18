292cfc37d2f8547e5d8e9e44eedc91bf29e0cf28
meaghanbass <meaghan.bass@gmail.com>
2020-02-18 15:19:30 -0800

    successfully imported dataset from external source

---
+++ src/components/charts/visChartComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class VisChartComponent extends React.Component {
+    render() {
+        
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default VisChartComponent
--- src/pages/charts.js
+++ src/pages/charts.js
@@ -2,12 +2,14 @@ import React from "react"
 import GeneralLayout from "../components/general-layout"
 import SEO from "../components/seo"
 import AMChartComponent from "../components/charts/amChartComponent"
+// import VisChartComponent from "../components/charts/visChartComponent"
 
 
 export default () => {
     return (
         <GeneralLayout>
             <SEO title="Charts" />
+            {/* <VisChartComponent /> */}
             <AMChartComponent />
         </GeneralLayout>
     )
