8801d957ee1e9e93e24ea200493318d5225f6049
meaghanbass <meaghan.bass@gmail.com>
2020-02-19 11:55:55 -0800

    that worked, try loading chart page

--- src/pages/charts.js
+++ src/pages/charts.js
@@ -1,8 +1,8 @@
 import React from "react"
 import GeneralLayout from "../components/general-layout"
 import SEO from "../components/seo"
-// import AMChartComponent from "../components/charts/amChartComponent"
-import D3ChartComponent from "../components/charts/d3ChartComponent"
+import AMChartComponent from "../components/charts/amChartComponent"
+// import D3ChartComponent from "../components/charts/d3ChartComponent"
 
 // I removed AMCHARTS because it was breaking on deployment
 // run npm install @amcharts/amcharts4 if you want to try to use it again. A chart component has already been created.
@@ -12,8 +12,8 @@ export default () => {
     return (
         <GeneralLayout>
             <SEO title="Charts" />
-            <D3ChartComponent />
-            {/* <AMChartComponent /> */}
+            {/* <D3ChartComponent /> */}
+            <AMChartComponent />
         </GeneralLayout>
     )
 }
